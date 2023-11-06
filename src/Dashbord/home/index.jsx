687e47b8800e63687d9013d04b5af4b3f46f93bf
Atik1000 <foysalkazi45@gmail.com>
2023-11-06 19:06:43 +0600

    dashbord home

--- src/Dashbord/home/index.jsx
+++ src/Dashbord/home/index.jsx
@@ -1,16 +1,59 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
 
-const DashboardHome = () => {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+
+  color: theme.palette.text.secondary,
+  height: 300,
+  width: 400,
+  borderRadius: 10,
+}));
+
+export default function DashboardHome() {
   return (
-    <div>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorum
-        ullam commodi tenetur corporis maxime, magni harum eum nemo odit
-        aspernatur laborum numquam temporibus. Dolorem labore magnam error
-        cumque eveniet?
-      </h1>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Item>
+            <Typography
+              variant="h2"
+              gutterBottom
+              sx={{ fontSize: 18, fontWeight: 700 }}
+            >
+              Total order
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <Typography
+              variant="h2"
+              gutterBottom
+              sx={{ fontSize: 18, fontWeight: 700 }}
+            >
+              Payment delevered
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <Typography
+              variant="h2"
+              gutterBottom
+              sx={{ fontSize: 18, fontWeight: 700 }}
+            >
+              Payment Pending
+            </Typography>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
-};
-
-export default DashboardHome;
+}
